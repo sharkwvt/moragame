8e2cadc00419d7a0516db4729181fdf1411f6af5
shark <shark@wvt.com.tw>
2025-03-13 16:40:16 +0800

    update

--- scene/start/start.gd
+++ scene/start/start.gd
@@ -5,8 +5,7 @@ class_name StartScene
 func _ready() -> void:
 	Main.current_scene = self
 	Main.instance_scenes[Main.SCENE.start] = self
-	Main.play_music(Main.music_1)
-	run_spine_test()
+	Main.play_music(Main.music_1)	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -18,10 +17,15 @@ func show_scene():
 	pass
 
 
-func run_spine_test():
-	var anim: SpineAnimationState = $SpineSprite.get_animation_state()
-	anim.set_animation("idle", true, 0)
-	anim.add_animation("run", 3, true, 0)
+func run_spine_start():
+	var bellaAnim: SpineAnimationState = $ColorRect/bella.get_animation_state()
+	bellaAnim.set_time_scale(2)
+	for element in (($ColorRect/bella.skeleton_data_res as SpineSkeletonDataResource).get_animations() as Array[SpineAnimation]) :
+		bellaAnim.add_animation((element as SpineAnimation).get_name(),0,false,0)
+	
+func run_spine_stop():
+	var anim: SpineAnimationState = $ColorRect/bella.get_animation_state()
+	anim.clear_tracks()
 
 
 func _on_start_button_pressed() -> void:
@@ -34,3 +38,13 @@ func _on_setting_button_pressed() -> void:
 
 func _on_結束_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_test_button_pressed() -> void:
+	$ColorRect.visible=true
+	run_spine_start()
+
+
+func _on_close_test_button_pressed() -> void:
+	$ColorRect.visible=false
+	run_spine_stop()
