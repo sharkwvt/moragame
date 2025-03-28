53975eaa0f80cb110dfff029c6b995c68345662e
unotony <tony.chiang@wvt.com.tw>
2025-03-28 14:57:39 +0800

    對話微調

--- common/talk/talk.gd
+++ common/talk/talk.gd
@@ -8,20 +8,20 @@ func _ready() -> void:
 
 
 func show_talk_anim(text: String):
+	# 初始化
 	visible = true
+	modulate.a = 0
 	var lbl: Label = $SubViewport/Label
 	lbl.text = text
-	$SubViewport.size.y = lbl.size.y
-	#size.y = lbl.size.y
-	#set_anchors_preset(Control.PRESET_BOTTOM_WIDE)
-	#position.y = Main.screen_size.y - size.y - 200
-	set_deferred("size", Vector2(size.x, lbl.size.y))
-	#set_deferred("position", Vector2(position.x, Main.screen_size.y - size.y - 200))
-	position.y = Main.screen_size.y - 200
-	modulate.a = 0
 	if tween:
 		tween.kill()
 	tween = create_tween()
+	
+	# 在下一幀後才會更新size
+	await get_tree().process_frame
+	$SubViewport.size.y = lbl.size.y
+	size.y = lbl.size.y
+	position.y = Main.screen_size.y - size.y - 200
 	tween.set_parallel(true)
 	tween.tween_property(self, "position:y", position.y + 50, 1)
 	tween.tween_property(self, "modulate:a", 1, 1)
