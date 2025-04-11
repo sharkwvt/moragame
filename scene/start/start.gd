0cdc02770df4d5c5ef9f9e85517317f04a6d7fb2
shark <shark@wvt.com.tw>
2025-04-11 16:13:10 +0800

    update start

--- scene/start/start.gd
+++ scene/start/start.gd
@@ -17,16 +17,16 @@ func _process(_delta: float) -> void:
 
 
 func setup():
-	var start_btn = $StartButton
+	var start_btn = $Control/StartButton
 	start_btn.mouse_entered.connect(_on_mouse_entered.bind(start_btn))
 	start_btn.mouse_exited.connect(_on_mouse_exited.bind(start_btn))
-	var review_btn = $ReviewButton
+	var review_btn = $Control/ReviewButton
 	review_btn.mouse_entered.connect(_on_mouse_entered.bind(review_btn))
 	review_btn.mouse_exited.connect(_on_mouse_exited.bind(review_btn))
-	var setting_btn = $SettingButton
+	var setting_btn = $Control/SettingButton
 	setting_btn.mouse_entered.connect(_on_mouse_entered.bind(setting_btn))
 	setting_btn.mouse_exited.connect(_on_mouse_exited.bind(setting_btn))
-	var exit_btn = $ExitButton
+	var exit_btn = $Control/ExitButton
 	exit_btn.mouse_entered.connect(_on_mouse_entered.bind(exit_btn))
 	exit_btn.mouse_exited.connect(_on_mouse_exited.bind(exit_btn))
 	var backgroundAni: SpineAnimationState = $SpineBG/SpineSprite.get_animation_state()
@@ -50,36 +50,6 @@ func show_scene():
 	pass
 
 
-func run_spine_start():
-	var bellaAnim: SpineAnimationState = $ColorRect/bella.get_animation_state()
-	bellaAnim.set_time_scale(2)
-	for element in (($ColorRect/bella.skeleton_data_res as SpineSkeletonDataResource).get_animations() as Array[SpineAnimation]) :
-		bellaAnim.add_animation((element as SpineAnimation).get_name(),0,false,0)
-	
-	var wendyAnim: SpineAnimationState = $ColorRect/wendy.get_animation_state()
-	wendyAnim.set_time_scale(2)
-	for element in (($ColorRect/wendy.skeleton_data_res as SpineSkeletonDataResource).get_animations() as Array[SpineAnimation]) :
-		wendyAnim.add_animation((element as SpineAnimation).get_name(),0,false,0)
-	
-	var lolaAnim: SpineAnimationState = $ColorRect/lola.get_animation_state()
-	lolaAnim.set_time_scale(2)
-	for element in (($ColorRect/lola.skeleton_data_res as SpineSkeletonDataResource).get_animations() as Array[SpineAnimation]) :
-		lolaAnim.add_animation((element as SpineAnimation).get_name(),0,false,0)
-	
-	var kittyAnim: SpineAnimationState = $ColorRect/kitty.get_animation_state()
-	kittyAnim.set_time_scale(2)
-	for element in (($ColorRect/kitty.skeleton_data_res as SpineSkeletonDataResource).get_animations() as Array[SpineAnimation]) :
-		kittyAnim.add_animation((element as SpineAnimation).get_name(),0,false,0)
-	
-func run_spine_stop():
-	var anim1: SpineAnimationState = $ColorRect/bella.get_animation_state()
-	anim1.clear_tracks()
-	var anim2: SpineAnimationState = $ColorRect/wendy.get_animation_state()
-	anim2.clear_tracks()
-	var anim3: SpineAnimationState = $ColorRect/kitty.get_animation_state()
-	anim3.clear_tracks()
-	var anim4: SpineAnimationState = $ColorRect/lola.get_animation_state()
-	anim4.clear_tracks()
 
 
 func _on_mouse_entered(btn: Button):
@@ -97,7 +67,7 @@ func _on_mouse_exited(btn: Button):
 		tween.kill()
 
 func _on_start_button_pressed() -> void:
-	var btn = $StartButton
+	var btn = $Control/StartButton
 	play_click_anim(btn)
 	#await get_tree().create_timer(0.3).timeout
 	await tween.finished
@@ -106,7 +76,7 @@ func _on_start_button_pressed() -> void:
 
 
 func _on_review_button_pressed() -> void:
-	var btn = $ReviewButton
+	var btn = $Control/ReviewButton
 	play_click_anim(btn)
 	await tween.finished
 	Main.to_scene(Main.SCENE.review)
@@ -114,7 +84,7 @@ func _on_review_button_pressed() -> void:
 
 
 func _on_setting_button_pressed() -> void:
-	var btn = $SettingButton
+	var btn = $Control/SettingButton
 	play_click_anim(btn)
 	await tween.finished
 	Main.show_setting_view()
@@ -122,17 +92,7 @@ func _on_setting_button_pressed() -> void:
 
 
 func _on_exit_pressed() -> void:
-	var btn = $ExitButton
+	var btn = $Control/ExitButton
 	play_click_anim(btn)
 	await tween.finished
 	get_tree().quit()
-
-
-func _on_test_button_pressed() -> void:
-	$ColorRect.visible=true
-	run_spine_start()
-
-
-func _on_close_test_button_pressed() -> void:
-	$ColorRect.visible=false
-	run_spine_stop()
