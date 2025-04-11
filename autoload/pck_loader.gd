87c5b34e7e34ac352abac829c86381504397c9b8
unotony <tony.chiang@wvt.com.tw>
2025-04-11 10:21:48 +0800

    新增pck讀取

---
+++ autoload/pck_loader.gd
@@ -0,0 +1,9 @@
+extends Node
+
+var pck_path = "res://pck_test.pck"
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	var success = ProjectSettings.load_resource_pack(pck_path, false)
+	if success:
+		print("pck讀取成功")
